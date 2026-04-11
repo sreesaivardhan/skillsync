9c47112d7cca1607f8f079b7f68cce23585eaf43
sreesaivardhan <sreesaivardhans@gmail.com>
2026-04-12 02:57:38 +0530

    fix: CORS production URL updated

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: [process.env.CLIENT_URL, 'http://localhost:5173'],
     credentials: true,
   })
 );
@@ -44,7 +44,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.CLIENT_URL,
+    origin: [process.env.CLIENT_URL, 'http://localhost:5173'],
     methods: ['GET', 'POST'],
     credentials: true,
   },
